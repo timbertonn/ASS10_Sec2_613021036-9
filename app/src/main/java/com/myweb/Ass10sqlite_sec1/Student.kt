b4904f12095de11b49236d6f6eaa80de3b6dd099
timbertonn <70010776+timbertonn@users.noreply.github.com>
2020-11-22 22:03:38 +0700

    ส่งงาน ASS10_Sec2_613021036-9

---
+++ app/src/main/java/com/myweb/Ass10sqlite_sec1/Student.kt
@@ -0,0 +1,4 @@
+package com.myweb.Ass10sqlite_sec1
+
+data class Student (val id:String, val name: String, val age: Int){
+}
---
+++ app/src/main/java/com/myweb/Ass10sqlite_sec1/StudentAdapter.kt
@@ -0,0 +1,33 @@
+package com.myweb.Ass10sqlite_sec1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.std_item_layout.view.*
+
+
+class StudentAdapter (val items : List<Student>, val context: Context) : RecyclerView.Adapter<StudentAdapter.ViewHolder>() {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvID?.text = "ID: "+items[position].id
+        holder.tvName?.text = "Name: "+items[position].name
+        holder.tvAge?.text = "Age: "+items[position].age.toString()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder   {
+        val view_item = LayoutInflater.from(parent.context).inflate(R.layout.std_item_layout, parent, false)
+        return ViewHolder(view_item)
+    }
+
+    override fun getItemCount(): Int {
+
+        return items.size
+    }
+
+    class ViewHolder (view: View): RecyclerView.ViewHolder(view){
+        val tvID = view.tv_id
+        val tvName = view.tv_name
+        val tvAge = view.tv_age
+    }
+}
